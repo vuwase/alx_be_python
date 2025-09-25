af19dd75f0fac86146750b4263e4d9550583b1f1
vuwase <v.uwase@alustudent.com>
2025-09-25 02:08:23 -0700

    changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,6 @@
-# fns_and_dsa/arithmetic_operations.py
-
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     """
     Performs basic arithmetic operations.
-
-    Parameters:
-        num1 (float): The first number.
-        num2 (float): The second number.
-        operation (str): The operation to perform ('add', 'subtract', 'multiply', 'divide').
-
-    Returns:
-        float | str: The result of the operation, or an error message if invalid.
     """
     if operation == "add":
         return num1 + num2
