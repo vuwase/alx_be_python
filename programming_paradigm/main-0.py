12233f9830c26b0fd5a029e7e0d8125165ed4d42
vuwase <v.uwase@alustudent.com>
2025-10-03 14:24:05 -0700

    fixed all errors

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -15,10 +15,10 @@ def main():
 
     if command == "deposit" and amount is not None:
         account.deposit(amount)
-        print(f"Deposited: ${amount:.2f}")
+        print(f"Deposited: ${amount}")
     elif command == "withdraw" and amount is not None:
         if account.withdraw(amount):
-            print(f"Withdrew: ${amount:.2f}")
+            print(f"Withdrew: ${amount}")
         else:
             print("Insufficient funds.")
     elif command == "display":
