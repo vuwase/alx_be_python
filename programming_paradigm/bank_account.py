fe638b5676f07a16bf91718ecd25f489bfdb5cc8
vuwase <v.uwase@alustudent.com>
2025-10-03 14:12:37 -0700

    fixed errors

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -21,6 +21,7 @@ class BankAccount:
         else:
             return False
 
-  def display_balance(self):
-    print(f"Current Balance: ${self._account_balance:.2f}")
+    def display_balance(self):
+        """Display the current balance."""
+        print(f"Current Balance: ${self.__account_balance}")
 
